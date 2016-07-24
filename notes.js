a19de8f985c383fe6254fd78df201c1bb5a99217
pavlo <notoriousb@bk.ru>
2016-07-25 01:36:51 +0300

    last fix

--- notes.js
+++ notes.js
@@ -1,24 +1,27 @@
 var User = React.createClass({
+
     render: function() {
         return (
-            <div className="note"> {this.props.children} </div>
+            <div className="note"> {this.props.children} 
+                <span className="delete-note" onClick={this.props.onDelete}> × </span>
+            </div>
         );
     }
 });
 
 
 var UsersGrid = React.createClass({
-    
-    
 
         render: function() {
+             var onNoteDelete = this.props.onNoteDelete;
         return (
-            <div className="notes-grid">
+            <div>
                 {
                   this.props.notes.map(function(note){
                         return (
                             <User
-                                key={note.id}>
+                                key={note.id}
+                                onDelete={onNoteDelete.bind(null, note)}>
                                 {note.name}
                             </User>
                         );
@@ -29,64 +32,66 @@ var UsersGrid = React.createClass({
     }   
 });
 
+
 var UserEditor = React.createClass({
+
     getInitialState: function() {
         return {
             text: ''
         };
     },
+
     handleTextChange: function(event) {
         this.setState({ text: event.target.value });
     },
 
     handleNoteAdd: function() {
+        if(!this.state.text) {return};
         var newNote = {
-            name: this.state.text,
-            id: Date.now()
+            id: Date.now(),
+            name: this.state.text
         };
+        console.log(newNote);
         this.props.onNoteAdd(newNote);
-    },
+        this.setState({ text: '' });
+    }, 
 
       render: function() {
         return (
-            <div className="note-editor">
-                <textarea
-                    placeholder="Enter your note here..."
-                    rows="10"
-                    className="textarea"
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter your name here..."
+                    
+                    className="input"
                     value={this.state.text}
                     onChange={this.handleTextChange}
                 />
                 <br />
-                <button className="add-button" onclick={this.hadleNoteAdd}>Add</button>
+                <button className="add-button" onClick={this.handleNoteAdd}>Add</button>
             </div>
         );
     }       
 });
 
+
 var UsersApp = React.createClass({
+
     getInitialState: function() {
         return {
-            notes: [
-            {
-                id:0,
-                name: "Ivan",
-            },
-            {
-                id:1,
-                name: "Ira",
-            },
-            {
-                id:2,
-                name: "Vasya",
-            },
-            {
-                id:3,
-                name: "Anya",
-            }
-
-        ]
+            notes: []
         };
+    },
+
+    componentDidMount: function() {
+        var localNotes = JSON.parse(localStorage.getItem('notes'));
+        if (localNotes) {
+            this.setState({ notes: localNotes });
+        }
+    },
+
+    componentDidUpdate: function() {
+        this._updateLocalStorage();
     },
     
     handleNoteAdd: function(newNote) {
@@ -95,14 +100,27 @@ var UsersApp = React.createClass({
         this.setState({ notes: newNotes });
     },
 
+    handleNoteDelete: function(note) {
+        var noteId = note.id;
+        var newNotes = this.state.notes.filter(function(note) {
+            return note.id !== noteId;
+        });
+        this.setState({ notes: newNotes });
+    },
+
     render: function() {
         return (
             <div className="note-app"> 
-                UsersApp
+            <h2>Users App</h2>
                 <UserEditor onNoteAdd={this.handleNoteAdd} />
-                <UsersGrid notes={this.state.notes}/>
+                <UsersGrid notes={this.state.notes} onNoteDelete={this.handleNoteDelete} />
             </div>
         );
+    },
+
+    _updateLocalStorage: function() {
+        var notes = JSON.stringify(this.state.notes);
+        localStorage.setItem('notes', notes);
     }   
 });
 
